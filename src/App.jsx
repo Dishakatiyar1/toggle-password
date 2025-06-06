1f666005bdda1e9a32fbd04094e969d8bfb3aeca
Disha Katiyar <77583219+Dishakatiyar1@users.noreply.github.com>
2025-06-06 17:53:35 +0530

    Initial commit

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const App = () => {
+  return (
+    <div className="flex w-full h-[100vh] mt-4 justify-center text-2xl font-bold">
+      Feature Placeholder
+    </div>
+  );
+};
+
+export default App;
